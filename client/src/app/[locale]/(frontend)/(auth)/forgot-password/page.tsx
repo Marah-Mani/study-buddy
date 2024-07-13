7447aadc1b681c0021268d63875c2e81d1dda990
Marah-Mani <marah.f.mani@gmail.com>
2024-07-13 11:22:36 +0530

    code add

--- client/src/app/[locale]/(frontend)/(auth)/forgot-password/page.tsx
+++ client/src/app/[locale]/(frontend)/(auth)/forgot-password/page.tsx
@@ -6,9 +6,9 @@ export default function Page() {
 	return (
 		<>
 			<div className='background-color'>
-				<Row gutter={[0, 0]}>
-					<Col xs={0} sm={0} md={0} lg={12} xl={12} xxl={12}>
-						<div className='back'></div></Col>
+				<Row gutter={[0, 0]} align='middle'>
+					<Col xs={0} sm={0} md={0} lg={12} xl={12} xxl={12} className='textCenter'>
+						<div> <img src='/images/logo.png' alt='' /> </div></Col>
 					<Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12}>
 						<div className="login-container">
 							<div className="login-content">
--- client/src/app/[locale]/(frontend)/(auth)/reset-password/page.tsx
+++ client/src/app/[locale]/(frontend)/(auth)/reset-password/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
 
 			<div className='background-color'>
 				<Row gutter={[0, 0]}>
-					<Col xs={0} sm={0} md={0} lg={12} xl={12} xxl={12}>
-						<div className='back'></div></Col>
+					<Col xs={0} sm={0} md={0} lg={12} xl={12} xxl={12} className='textCenter'>
+						<div> <img src='/images/logo.png' alt='' /> </div></Col>
 					<Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12}>
 						<div className="login-container">
 							<div className="login-content">
