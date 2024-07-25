02cea9c0604d56e8547e5e7e6171be51aba5973d
Marah-Mani <marah.f.mani@gmail.com>
2024-07-25 10:29:47 +0530

    take latest pull

--- check-pull.sh
+++ check-pull.sh
@@ -6,13 +6,10 @@ git fetch origin
 # Get the current branch name
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-# Compare the local branch with the remote development branch
-LOCAL=$(git rev-parse HEAD)
-REMOTE=$(git rev-parse origin/development)
-
-if [ "$LOCAL" != "$REMOTE" ]; then
+# Check if the current branch contains the latest commit from the remote development branch
+if git merge-base --is-ancestor origin/development "$branch"; then
+    echo -e "\e[32mYour branch is up to date with the development branch.\e[0m"
+else
     echo -e "\e[31mYour branch is not up to date with the development branch. Please pull the latest changes from development.\e[0m"
     exit 1
-else
-    echo -e "\e[32mYour branch is up to date with the development branch.\e[0m"
 fi
