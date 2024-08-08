934cb03e5969659c28ba92616dc994290f36907f
Marah-Mani <marah.f.mani@gmail.com>
2024-08-08 15:39:36 +0530

    chat issue of fullscreen

--- client/src/app/[locale]/layout.tsx
+++ client/src/app/[locale]/layout.tsx
@@ -15,6 +15,7 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
 	return (
 		<html>
 			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
 				<meta name="apple-mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 			</head>
